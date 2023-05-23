9c527e7233137617412398b8d863560130a1d235
Elias Elimah <elias.elimah@axazara.com>
2023-05-23 06:00:22 +0100

    Add payment en payout facade

---
+++ src/Facades/PaymentFacade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AxaZara\Moneroo\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PaymentFacade extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return 'payment';
+    }
+}
---
+++ src/Facades/PayoutFacade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AxaZara\Moneroo\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PayoutFacade extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return 'payout';
+    }
+}
